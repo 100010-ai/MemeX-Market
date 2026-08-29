ff50f29171049df58851af350b9d3d2416ff0b9c
Haze <reyzin378@gmail.com>
2026-08-29 22:37:33 +0300

    feat: show MXM Score on gift detail

--- app/gifts/[id]/page.tsx
+++ app/gifts/[id]/page.tsx
@@ -2,12 +2,9 @@
 
 import { useParams } from "next/navigation";
 import { GiftDetail } from "@/components/gifts/gift-detail";
+import { MXMScoreCard } from "@/components/gifts/mxm-score-card";
 
 export default function GiftPage() {
   const { id } = useParams<{ id: string }>();
-  return (
-    <div className="mx-auto max-w-5xl">
-      <GiftDetail id={id} />
-    </div>
-  );
+  return <div className="mx-auto max-w-5xl"><MXMScoreCard giftId={id} /><GiftDetail id={id} /></div>;
 }
